687186bdd839da3840f05e14be417567519dbc33
Jesus Lomeli <jesus.lomeli@arrow.com>
2020-01-06 14:58:41 -0600

    Created the Tracklisting componenet

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -14,6 +14,7 @@ export class ProductDescriptionComponent implements OnInit {
 
   ngOnInit() {
     this._productService
-      .getAlbum(1).subscribe(response => (this.albumInfo = response));
+      .getAlbum(1)
+      .subscribe(response => (this.albumInfo = response));
   }
 }
